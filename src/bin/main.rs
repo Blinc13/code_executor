774a11b97426884029e327955e958830db03d64b
Blinc <65546512+Blinc13@users.noreply.github.com>
2023-01-09 13:36:39 +0300

    Init
    -- Basic file structure
    -- Some code in main.rs

---
+++ src/bin/main.rs
@@ -0,0 +1,25 @@
+use serenity::{
+    client,
+    prelude::GatewayIntents
+};
+use compile_bot::EventHandler;
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() {
+    FmtSubscriber::builder()
+        .init();
+
+    let token = std::env::var("TOKEN").expect("Failed to get token from env. Maybe you not provided one?");
+
+    let intents = GatewayIntents::DIRECT_MESSAGES;
+    let mut client = client::Client::builder(token, intents)
+        .event_handler(EventHandler::from_env())
+        .await.expect("Failed to construct client");
+
+    tracing::info!("Client built!");
+
+    if let Err(e) = client.start().await {
+        tracing::error!("Client error: {e}");
+    }
+}
